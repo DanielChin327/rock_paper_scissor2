1fc871c783df7ada1964a1eb5324b0419bc65324
Daniel Chin <DanChin327@gmail.com>
2024-06-05 14:24:47 -0600

    adding JS

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+const choice = ['rock', 'paper', 'scissor']
+
+
+
+const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('.paper');
+const scissorButton = document.querySelector('.scissor');
+
+rockButton.addEventListener('click', () => {
+  playGame('rock');
+});
+paperButton.addEventListener('click', () => {
+  playGame('paper');
+});
+scissorButton.addEventListener('click', () => {
+  playGame('scissor');
+});
+
+function getComputerChoice(){
+  const randomIndex = Math.floor(Math.random() * choice.length)
+}
+
+
+
+
+
+
+
+let playerScore = 0;
 let computerScore = 0;
